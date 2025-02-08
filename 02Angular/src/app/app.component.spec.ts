56bd2dc20f3be8d3333237212f255ad2d443e187
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-08 05:31:45 +0500

    almost done

--- 02Angular/src/app/app.component.spec.ts
+++ 02Angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '02Angular' title`, () => {
+  it(`should have the 'myApp2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('02Angular');
+    expect(app.title).toEqual('myApp2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 02Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myApp2');
   });
 });
--- 02Angular/src/app/app.component.ts
+++ 02Angular/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02Angular';
+  title = 'Class 11B';
 }
